3388aee59238ddc3c5f4c48b2454a27d757c45ce
AlexSaenen <alexander.saenen@epitech.eu>
2017-02-28 18:39:35 -0800

    Update

--- Assignment_2/sender.cpp
+++ Assignment_2/sender.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <cstring>
 #include <unistd.h>
+
+#define REGULAR 1
 
 using namespace std;
 
+typedef struct message {
+  long mtype;
+  char *mtext;
+} message;
+
+int getMessageQueue(int identifier, msqid_ds *queue) {
+  return msgctl(identifier, IPC_STAT, queue);
+}
+
 int main() {
   string input;
+  msqid_ds queue;
 
   cout << "Hi, I'm the sender (PID: " << getpid() << "), ";
   cout << "Let me send something : ";
   cin >> input;
+
+  if (getMessageQueue(IPC_PRIVATE, &queue) == -1) {
+    cout << "Sender failed to get message queue" << endl;
+  }
+
+  message newMsg;
+  newMsg.mtype = REGULAR;
+  newMsg.mtext = new char [input.length() + 1];
+  strcpy(newMsg.mtext, input.c_str());
+
+  msgsnd(IPC_PRIVATE, &newMsg, input.length(), 0);
+
+  delete [] newMsg.mtext;
+
   return 0;
 }
