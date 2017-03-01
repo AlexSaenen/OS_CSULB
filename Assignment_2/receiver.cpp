18f8b6d1c96645f64b9a27273c54a4074b19fa2e
AlexSaenen <alexander.saenen@epitech.eu>
2017-03-01 14:32:54 -0800

    Update

--- Assignment_2/receiver.cpp
+++ Assignment_2/receiver.cpp
@@ -28,7 +28,9 @@ int main(int argc, char *argv[]) {
   int ret = msgrcv(messageQueueID, &newMsg, MAX_MESSAGE_SIZE, REGULAR, MSG_NOERROR);
 
   cout << ret << endl;
-  
+  cout << "hello" << endl;
+  cout << newMsg.mtype << endl;
+
   newMsg.mtext[ret] = 0;
 
   // cout << "I'm the receiver (PID: " << getpid() << "), and got a new message:" << endl;
