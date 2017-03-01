8a23c2d6905d37d44497cf7bdd62fd3cd814bc64
AlexSaenen <alexander.saenen@epitech.eu>
2017-03-01 14:46:27 -0800

    Update

--- Assignment_2/receiver.cpp
+++ Assignment_2/receiver.cpp
@@ -20,24 +20,12 @@ int main(int argc, char *argv[]) {
   }
 
   int messageQueueID = atoi(argv[1]);
-
   message newMsg;
 
-  int ret = msgrcv(messageQueueID, &newMsg, sizeof(newMsg.mtext), 0, 0);
+  msgrcv(messageQueueID, &newMsg, sizeof(newMsg.mtext), 0, 0);
 
-  cout << ret << endl;
-  cout << "working ?" << endl;
-  cout << newMsg.mtype << endl;
-  cout << newMsg.mtext[0] << endl;
-
-  newMsg.mtext[ret] = 0;
-
-  // cout << "I'm the receiver (PID: " << getpid() << "), and got a new message:" << endl;
-  cout << newMsg.mtext[0] << endl;
-  cout << "text " << newMsg.mtext << " end text" << endl;
-  // cout << "wtf is going onnnnnnnn" << endl;
-  //
-  // delete [] newMsg.mtext;
+  cout << "I'm the receiver (PID: " << getpid() << "), and got a new message:" << endl;
+  cout << newMsg.mtext << endl;
 
   return 0;
 }
