9a256527cd6d3da6d899d929c6e58205f21f2ec8
AlexSaenen <alexander.saenen@epitech.eu>
2017-02-28 17:15:26 -0800

    Sending arguments

--- Assignment_2/KernelClasses/Child.cpp
+++ Assignment_2/KernelClasses/Child.cpp
@@ -11,7 +11,13 @@ Child::Child(string binaryName) {
 
   if (this->isParent() == false) {
     string pathToBinary("./" + binaryName);
-    execv(pathToBinary.c_str(), 0);
+
+    char arguments = [2][7] = {
+      pathToBinary.c_str(),
+      0,
+    };
+
+    execv(pathToBinary.c_str(), arguments);
     throw "Failed to exec";
   }
 }
